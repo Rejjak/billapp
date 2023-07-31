f33ed199a0e413d3f389178dbd2fa054b8c79570
Rejjak <rejjakali94@gmail.com>
2023-07-30 23:52:44 -0700

    initial code

--- src/component/bag/printPage.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useState, useCallback, useEffect,memo,useContext,useRef } from 'react';
-
-const PrintComponent = React.forwardRef((props, ref) => {
-    return (
-      <div ref={ref}>My cool content here!</div>
-    );
-})
-
-export default PrintComponent;
